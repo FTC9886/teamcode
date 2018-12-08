16269f2e9246336160729a4ef98ce2cded4bb7b4
FTC-9886 <45716516+FTC-9886@users.noreply.github.com>
2018-12-08 09:52:55 -0600

    Add files via upload

---
+++ Hardware.java
@@ -0,0 +1,182 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/*import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+*/
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ *  NEED TO UPDATE THE FOLLOWING NOTES ....
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+public class Hardware
+{
+    /* Public OpMode members. */
+    public DcMotor  left_front_drive   = null;
+    public DcMotor  right_front_drive  = null;
+    public DcMotor  left_back_drive    = null;
+    public DcMotor  right_back_drive   = null;
+
+    public DcMotor hang_arm;
+    public DcMotor extend_arm;
+    public DcMotor rotate_arm;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: Rev Hex Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.1 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    /* Constructor */
+    public Hardware()
+    {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        left_front_drive  = hwMap.dcMotor.get("left_front_drive");
+        right_front_drive = hwMap.dcMotor.get("right_front_drive");
+        left_back_drive   = hwMap.dcMotor.get("left_back_drive");
+        right_back_drive  = hwMap.dcMotor.get("right_back_drive");
+
+        hang_arm = hwMap.dcMotor.get("hang_arm");
+        extend_arm = hwMap.dcMotor.get("extend_arm");
+        rotate_arm = hwMap.dcMotor.get("rotate_arm");
+
+        left_front_drive.setDirection(DcMotor.Direction.FORWARD);
+        right_front_drive.setDirection(DcMotor.Direction.FORWARD);
+        left_back_drive.setDirection(DcMotor.Direction.FORWARD);
+        right_back_drive.setDirection(DcMotor.Direction.FORWARD);
+
+        hang_arm.setDirection(DcMotor.Direction.FORWARD);
+        extend_arm.setDirection(DcMotor.Direction.FORWARD);
+        rotate_arm.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        left_front_drive.setPower(0);
+        right_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_back_drive.setPower(0);
+        hang_arm.setPower(0);
+        extend_arm.setPower(0);
+        rotate_arm.setPower(0);
+
+        // Stop and reset all encoders
+        right_front_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left_front_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Set motors to run with / without encoders
+        left_front_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+    public void drive (LinearOpMode opMode, double inches, double speed, double timeoutS)
+    {
+        int newLeftFrontTarget  = left_front_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+        int newRightFrontTarget = right_front_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+        left_front_drive.setTargetPosition(newLeftFrontTarget);
+        right_front_drive.setTargetPosition(newRightFrontTarget);
+        int newLeftBackTarget  = left_back_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+        int newRightBackTarget = right_back_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+        left_back_drive.setTargetPosition(newLeftBackTarget);
+        right_back_drive.setTargetPosition(newRightBackTarget);
+
+        // Turn On RUN_TO_POSITION
+        left_front_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion
+        ElapsedTime     runtime = new ElapsedTime();
+        runtime.reset();
+        left_front_drive.setPower(Math.abs(speed));
+        right_front_drive.setPower(Math.abs(speed));
+        left_back_drive.setPower(Math.abs(speed));
+        right_back_drive.setPower(Math.abs(speed));
+
+        while (opMode.opModeIsActive() &&
+                (runtime.seconds() < timeoutS) &&
+                (left_front_drive.isBusy() && right_front_drive.isBusy()))
+        {
+        }
+
+        // Stop all motion;
+        left_front_drive.setPower(0);
+        right_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
+
---
+++ Teleop_Tank_v3.java
@@ -0,0 +1,186 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * This file provides basic Telop driving for a Pushbot robot.
+ * The code is structured as an Iterative OpMode
+ *
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ *
+ * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Mec_Tank_v3", group="Mec_Tank_v3")
+//@Disabled
+public class Teleop_Tank_v3 extends OpMode{
+
+    /* Declare OpMode members. */
+    Hardware robot       = new Hardware(); // use the class created to define a Pushbot's hardware
+                                                         // could also use HardwarePushbotMatrix class.
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     * Code to run ONCE when the driver hits INIT
+     * Code to run ONCE when the driver hits INIT
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "hello Driver");    //
+        updateTelemetry(telemetry);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        // ********************************************************
+        // CONTROLLER #1 DRIVER
+        // ********************************************************
+        // Tank drive and translate controls
+        double gamepad1_left  = 0.0;
+        double gamepad1_right = 0.0;
+        gamepad1_left = gamepad1.left_stick_y;
+        gamepad1_right = -gamepad1.right_stick_y;
+        if(gamepad1_left > 0.1 || gamepad1_left < -0.1 || gamepad1_right > 0.1 || gamepad1_right < -0.1)
+        {
+            robot.left_front_drive.setPower (gamepad1_left);
+            robot.right_front_drive.setPower(gamepad1_right);
+            robot.left_back_drive.setPower  (gamepad1_left);
+            robot.right_back_drive.setPower (gamepad1_right);
+        }
+
+//        else if (gamepad1.dpad_up)
+//        {
+//            robot.left_front_drive.setPower (0.3);
+//            robot.right_front_drive.setPower(0.3);
+//        }
+//        else if (gamepad1.dpad_down)
+//        {
+//            robot.left_front_drive.setPower (-0.3);
+//            robot.right_front_drive.setPower(-0.3);
+//        }
+
+        else if (gamepad1.right_trigger > 0.1)
+        {
+            robot.left_front_drive.setPower (-gamepad1.right_trigger/1.5);
+            robot.right_front_drive.setPower(-gamepad1.right_trigger/1.5);
+            robot.left_back_drive.setPower  (gamepad1.right_trigger/1.5);
+            robot.right_back_drive.setPower (gamepad1.right_trigger/1.5);
+        }
+        else if (gamepad1.left_trigger > 0.1)
+        {
+            robot.left_front_drive.setPower (gamepad1.left_trigger/1.5);
+            robot.right_front_drive.setPower(gamepad1.left_trigger/1.5);
+            robot.left_back_drive.setPower  (-gamepad1.left_trigger/1.5);
+            robot.right_back_drive.setPower (-gamepad1.left_trigger/1.5);
+        }
+        else
+        {
+            robot.left_front_drive.setPower (0f);
+            robot.right_front_drive.setPower(0f);
+            robot.left_back_drive.setPower  (0f);
+            robot.right_back_drive.setPower (0f);
+        }
+
+
+        // to move the lift/hang arm up and down. left bumper is up and right bumper is down.
+        if (gamepad1.left_bumper){
+            robot.hang_arm.setPower(0.75);
+        }else if (gamepad1.right_bumper){
+            robot.hang_arm.setPower(-0.75);
+        } else {
+            robot.hang_arm.setPower(0);
+        }
+
+        // to extend and contract the collection arm. "b" is extending and "x" is contracting.
+        if (gamepad1.b){
+            robot.extend_arm.setPower(0.80);
+        }else if (gamepad1.x){
+            robot.extend_arm.setPower(-0.80);
+        } else {
+            robot.extend_arm.setPower(0);
+        }
+
+        // to rotate the collection arm up and down. "a" is to rotate up and "y" is to rotate downward.
+        if (gamepad1.a){
+            robot.rotate_arm.setPower(0.75);
+        }else if (gamepad1.y){
+            robot.rotate_arm.setPower(-0.75);
+        } else {
+            robot.rotate_arm.setPower(0);
+        }
+
+        updateTelemetry(telemetry);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop()
+    {
+    }
+}
