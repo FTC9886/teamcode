ef8b4331525ab79577baf203d7c7917da9b62c63
FTC9886 <ftc9886@gmail.com>
2018-12-22 09:51:08 -0600

    Created Collector subsystem class

---
+++ Collector.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Collector {
+
+    private DcMotor collector;
+
+
+    private enum CollectorEnum {
+        COLLECTING,
+        EJECTING,
+        STOPPED
+    }
+
+    private CollectorEnum collectorState;
+
+    public Collector(String collector_motor, HardwareMap hardwareMap){
+        this.collector = hardwareMap.dcMotor.get(collector_motor);
+
+        stop();
+    }
+
+    public void collect(){
+        collector.setPower(1);
+        collectorState = CollectorEnum.COLLECTING;
+    }
+
+    public void eject(){
+        collector.setPower(-1);
+        collectorState = CollectorEnum.EJECTING;
+    }
+
+    public void stop(){
+        collector.setPower(0);
+        collectorState = CollectorEnum.STOPPED;
+    }
+
+}
