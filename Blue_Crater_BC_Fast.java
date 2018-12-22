92cd7beeb512f55dd038401405b910647b3efbc7
JPAshour <jpashour@gmail.com>
2018-12-21 20:59:25 -0600

    Created CombinedHardware class to be used instead of seperate AutoHardware and Hardware classes, modified Blue Crater auto to use new CombinedHardware Class

--- Blue_Crater_BC_Fast.java
+++ Blue_Crater_BC_Fast.java
@@ -3,9 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
 @TeleOp(name="Blue_Crater_BC_Fast", group="Autonomous")
 //@Disabled
 
@@ -14,7 +11,7 @@ public class Blue_Crater_BC_Fast extends LinearOpMode {
 
 
         /* Declare OpMode members. */
-        Hardware robot       = new Hardware(); // use the class created to define a Pushbot's hardware
+        CombinedHardware robot = new CombinedHardware(); // use the class created to define a Pushbot's hardware
         // could also use HardwarePushbotMatrix class.
 
         /* Initialize the hardware variables.
@@ -22,29 +19,144 @@ public class Blue_Crater_BC_Fast extends LinearOpMode {
          */
         robot.init(hardwareMap);
         Michaels_tensor_flow tensor_flow = new Michaels_tensor_flow();
-        tensor_flow.init(this );
+        tensor_flow.init(this);
         waitForStart();
-        robot.hang_arm.setPower(0.75);
+        //change to use a more robust function later
+        robot.hangArm.lower();
         robot.pause(5000);
-        robot.hang_arm.setPower(0.0);
+        robot.hangArm.stop();
 
+
+        //Move robot away from lander latch
+
+        robot.driveRight(this, 3, 0.25, 3);
+
+        //Drive right to clear lander
 
         tensor_flow.activate();
         //FIND GOLD
         Michaels_tensor_flow.goldfinder goldPosition = tensor_flow.getGoldPosition(telemetry);
 
+        //Robot chooses which path to take, Left, Right, Middle, or Unknown
+
+        switch (goldPosition) {
+            case LEFT:
+                //drive 2" from minerals
+                robot.translateForward(this, 30, 0.50, 5);
+                //drive to left block
+                robot.driveLeft(this, 14, 0.50, 5);
+                //push left block
+                robot.translateForward(this, 4, 0.50, 3);
+                //turn robot to the left to face the wall
+                    // robot.turnLeft(90 *);
+                //drive toward the wall
+                robot.translateForward(this, 17, 0.30, 5);
+                //turn to be parallel with wall
+                    //robot.turnLeft(45 *);
+                //drive to depot
+                robot.translateForward(this, 60, 0.50, 10);
+                //deposit marker
+                    //robot.markerServo.setPosition(x);
+                //drive to crater
+                robot.translateForward(this, 74, 0.75, 10);
+                break;
+
+            case RIGHT:
+                //drive 2" from minerals
+                robot.translateForward(this, 30, 0.50, 5);
+                //drive to right block
+                robot.driveRight(this, 11, 0.50, 5);
+                //push right block
+                robot.translateForward(this, 4, 0.50, 3);
+                //back up
+                robot.translateBackward(this, 6, 0.50, 3);
+                //turn robot left to face the wall
+                    //robot.turnLeft(90 *);
+                //drive toward the wall
+                robot.translateForward(this, 100, 0.75, 10);
+                //turn to be parallel with the wall
+                    //robot.turnLeft(45 *);
+                //drive to depot
+                robot.translateForward(this, 36, 0.50, 5);
+                //drive to crater
+                robot.translateForward(this, 74, 0.75, 10);
+                break;
+
+            case MIDDLE:
+                //drive 2" from minerals
+                robot.translateForward(this, 30, 0.50, 10);
+                //push middle block
+                robot.translateForward(this, 4, 0.50, 3);
+                //back up
+                robot.translateForward(this, 6, 0.50, 3);
+                //turn robot left to face the wall
+                    //robot.turnLeft(90 *);
+                //drive toward the wall
+                robot.translateForward(this, 67, 0.75, 10);
+                //turn to be parallel with the wall
+                    //robot.turnLeft(45 *);
+                //drive to depot
+                robot.translateForward(this, 36, 0.50, 5);
+                //drive to crater
+                robot.translateForward(this, 74, 0.75, 10);
+                break;
+
+            case UNKNOWN:
+            default: //like unknown
+                //drive strait and hope the block is there
+                break;
+
+
+        }
+
         tensor_flow.deactivate();
 
-        //lower down
-        robot.driveRight(this, 10, 0.5, 5);
+        telemetry.addData("Path0", "Starting at %7d : %7d : %7d : %7d",
+                robot.left_front_drive.getCurrentPosition(),
+                robot.right_front_drive.getCurrentPosition(),
+                robot.left_back_drive.getCurrentPosition(),
+                robot.right_back_drive.getCurrentPosition());
+        telemetry.update();
 
-        robot.pause(30000);
+        robot.driveRight(this, 36, 0.25, 5);
+        telemetry.addData("Path0", "Starting at %7d : %7d : %7d : %7d",
+                robot.left_front_drive.getCurrentPosition(),
+                robot.right_front_drive.getCurrentPosition(),
+                robot.left_back_drive.getCurrentPosition(),
+                robot.right_back_drive.getCurrentPosition());
+        telemetry.update();
+        robot.pause(15000);
 
-        robot.driveLeft(this, 10, 0.5, 5);
+        robot.driveLeft(this, 36, 0.25, 5);
+        telemetry.addData("Path0", "Starting at %7d : %7d : %7d : %7d",
+                robot.left_front_drive.getCurrentPosition(),
+                robot.right_front_drive.getCurrentPosition(),
+                robot.left_back_drive.getCurrentPosition(),
+                robot.right_back_drive.getCurrentPosition());
+        telemetry.update();
+        robot.pause(15000);
 
-        //robot.driveForward(this, 10, 0.5, 5);
+        robot.translateForward(this, 36, 0.25, 5);
+        telemetry.addData("Path0", "Starting at %7d : %7d : %7d : %7d",
+                robot.left_front_drive.getCurrentPosition(),
+                robot.right_front_drive.getCurrentPosition(),
+                robot.left_back_drive.getCurrentPosition(),
+                robot.right_back_drive.getCurrentPosition());
+        telemetry.update();
+        robot.pause(15000);
 
-        //robot.driveBackward(this, 10, 0.5, 5);
+        robot.translateBackward(this, 36, 0.25, 5);
+        telemetry.addData("Path0", "Starting at %7d : %7d : %7d : %7d",
+                robot.left_front_drive.getCurrentPosition(),
+                robot.right_front_drive.getCurrentPosition(),
+                robot.left_back_drive.getCurrentPosition(),
+                robot.right_back_drive.getCurrentPosition());
+        telemetry.update();
+        robot.pause(15000);
+
+        //robot.translateForward(this, 10, 0.5, 5);
+
+        //robot.translateBackward(this, 10, 0.5, 5);
 
         //delatch
         //scan for the minearals
---
+++ CombinedHardware.java
@@ -0,0 +1,218 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class CombinedHardware {
+
+    //Define Drivetrain
+    public DcMotor  left_front_drive   = null;
+    public DcMotor  right_front_drive  = null;
+    public DcMotor  left_back_drive    = null;
+    public DcMotor  right_back_drive   = null;
+
+    //Define other subsystems/motors
+    public HangArm hangArm;
+    public ExtenderArm extenderArm;
+    public RotateArm rotateArm;
+
+    //Define Servos
+
+    //Define Sensors
+    public BNO055IMU adafruitIMU;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+
+
+    /* Constructor */
+    public CombinedHardware()
+    {
+    }
+
+    public void init(HardwareMap ahwMap){
+        hwMap = ahwMap;
+
+        DcMotor left_front_drive = ahwMap.dcMotor.get("left_front_drive");
+        DcMotor right_front_drive = ahwMap.dcMotor.get("right_front_drive");
+        DcMotor left_back_drive = ahwMap.dcMotor.get("left_back_drive");
+        DcMotor right_back_drive = ahwMap.dcMotor.get("right_back_drive");
+
+        left_front_drive.setDirection(DcMotor.Direction.FORWARD);
+        right_front_drive.setDirection(DcMotor.Direction.FORWARD);
+        left_back_drive.setDirection(DcMotor.Direction.FORWARD);
+        right_back_drive.setDirection(DcMotor.Direction.FORWARD);
+
+        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        hangArm = new HangArm("hang_arm","upper_touch","lower_touch", ahwMap);
+        extenderArm = new ExtenderArm("extend_arm", ahwMap);
+        rotateArm = new RotateArm("rotate_arm", ahwMap);
+
+        //Sensor Setup
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        adafruitIMU = ahwMap.get(BNO055IMU.class, "imu 1");
+        adafruitIMU.initialize(parameters);
+    }
+
+    private ElapsedTime period  = new ElapsedTime();
+
+    /*Value definitions to be used for distance conversion*/
+    static final double     COUNTS_PER_MOTOR_REV    = 653 ;    // eg: Rev Hex Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.1 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 6.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH_LandR   = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1416);
+    static final double     COUNTS_PER_INCH_FandB   = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1416) /(2);
+
+    //Used to power the right side of the drive for the Gyro Auto Driver
+    public void rightDrivePower(double power){
+        left_front_drive.setPower(power);
+        right_front_drive.setPower(power);
+    }
+    //Used to power the left side of the drivetrain for the Gyro Auto Driver
+    public void leftDrivePower(double power){
+        left_back_drive.setPower(power);
+        right_back_drive.setPower(power);
+    }
+    //Used to set power for both sides of the drivetrain at once
+    public void drivePowers(double leftSpeed, double rightSpeed){
+        leftDrivePower(leftSpeed);
+        rightDrivePower(rightSpeed);
+    }
+    //Stops the DriveTrain
+    public void stopDrive(){
+        drivePowers(0.0, 0.0);
+    }
+
+
+    /*Functions used in current autonomous programs*/
+    public void driveLeft (LinearOpMode opMode, double inches, double speed, double timeoutS)
+    {
+        driveRight(opMode, -inches, speed, timeoutS);
+    }
+    public void driveRight (LinearOpMode opMode, double inches, double speed, double timeoutS)
+    {
+        int newLeftFrontTarget  = left_front_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_LandR);
+        int newRightFrontTarget = right_front_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_FandB);
+        left_front_drive.setTargetPosition(newLeftFrontTarget);
+        right_front_drive.setTargetPosition(newRightFrontTarget);
+        int newLeftBackTarget  = left_back_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_LandR);
+        int newRightBackTarget = right_back_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_FandB);
+        left_back_drive.setTargetPosition(newLeftBackTarget);
+        right_back_drive.setTargetPosition(newRightBackTarget);
+
+        // Turn On RUN_TO_POSITION
+        left_front_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion
+        ElapsedTime     runtime = new ElapsedTime();
+        runtime.reset();
+        left_front_drive.setPower(Math.abs(speed));
+        right_front_drive.setPower(Math.abs(speed));
+        left_back_drive.setPower(Math.abs(speed));
+        right_back_drive.setPower(Math.abs(speed));
+
+        while (opMode.opModeIsActive() &&
+                (runtime.seconds() < timeoutS) &&
+                (left_front_drive.isBusy() && right_front_drive.isBusy()))
+        {
+        }
+
+        // Stop all motion;
+        left_front_drive.setPower(0);
+        right_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void translateBackward(LinearOpMode opMode, double inches, double speed, double timeoutS)
+    {
+        translateForward(opMode, -inches, speed, timeoutS);
+    }
+
+    public void translateForward(LinearOpMode opMode, double inches, double speed, double timeoutS)
+    {
+        int newLeftFrontTarget  = left_front_drive.getCurrentPosition() + (int)(-inches * COUNTS_PER_INCH_LandR);
+        int newRightFrontTarget = right_front_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_FandB);
+        left_front_drive.setTargetPosition(newLeftFrontTarget);
+        right_front_drive.setTargetPosition(newRightFrontTarget);
+        int newLeftBackTarget  = left_back_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_LandR);
+        int newRightBackTarget = right_back_drive.getCurrentPosition() + (int)(-inches * COUNTS_PER_INCH_FandB);
+        left_back_drive.setTargetPosition(newLeftBackTarget);
+        right_back_drive.setTargetPosition(newRightBackTarget);
+
+        // Turn On RUN_TO_POSITION
+        left_front_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion
+        ElapsedTime     runtime = new ElapsedTime();
+        runtime.reset();
+        left_front_drive.setPower(Math.abs(speed));
+        right_front_drive.setPower(Math.abs(speed));
+        left_back_drive.setPower(Math.abs(speed));
+        right_back_drive.setPower(Math.abs(speed));
+
+        while (opMode.opModeIsActive() &&
+                (runtime.seconds() < timeoutS) &&
+                (left_front_drive.isBusy() && right_front_drive.isBusy()))
+        {
+        }
+
+        // Stop all motion;
+        left_front_drive.setPower(0);
+        right_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void turnLeft(LinearOpMode opMode, double inches, double speed, double timeoutS){
+
+    }
+
+    public void turnRight(){
+        
+    }
+
+    //Sleep while catching InteruptedException
+    public void pause(long millis )
+    {
+        try {
+            Thread.sleep(millis );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
