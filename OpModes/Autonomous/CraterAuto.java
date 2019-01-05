3e41654a11aa89bcd370dd9769478e53d0e43e78
FTC9886 <ftc9886@gmail.com>
2019-01-05 10:46:27 -0600

    Commented out movement for unknown position

--- OpModes/Autonomous/CraterAuto.java
+++ OpModes/Autonomous/CraterAuto.java
@@ -31,189 +31,189 @@ public class CraterAuto extends LinearOpMode {
         ElapsedTime timer = new ElapsedTime();
 
         waitForStart();
-        //change to use a more robust function later
-        while(!robot.hangArm.isFullyLowered()) {
-            robot.hangArm.lower();
-        }
-        robot.hangArm.stop();
-        robot.pause(1000);
+            //change to use a more robust function later
+            while (!robot.hangArm.isFullyLowered()) {
+                robot.hangArm.lower();
+            }
+            robot.hangArm.stop();
+            robot.pause(1000);
 
-        //Move robot away from lander latch
-        robot.driveTrain.translateBackward(this, 4, 0.3, 3);
-        robot.pause(500);
+            //Move robot away from lander latch
+            robot.driveTrain.translateBackward(this, 4, 0.3, 3);
+            robot.pause(500);
 
-        tensor_flow.activate();
-        //FIND GOLD
-        Michaels_tensor_flow.goldfinder goldPosition = Michaels_tensor_flow.goldfinder.UNKNOWN;
-        timer.startTime();
-        while(goldPosition == Michaels_tensor_flow.goldfinder.UNKNOWN) {
-            goldPosition = tensor_flow.getGoldPosition(telemetry);
+            tensor_flow.activate();
+            //FIND GOLD
+            Michaels_tensor_flow.goldfinder goldPosition = Michaels_tensor_flow.goldfinder.UNKNOWN;
+            timer.reset();
+            while (goldPosition == Michaels_tensor_flow.goldfinder.UNKNOWN && timer.seconds() < 5) {
+                goldPosition = tensor_flow.getGoldPosition(telemetry);
 //            if(timer.seconds()>15){
 //                break;
 //            }
-        }
-        telemetry.addData("Position", goldPosition);
-        telemetry.update();
-        robot.pause(1000);
-        tensor_flow.deactivate();
+            }
+            telemetry.addData("Position", goldPosition);
+            telemetry.update();
+            robot.pause(1000);
+            tensor_flow.deactivate();
 
-        //Robot chooses which path to take, Left, Right, Middle, or Unknown
+            //Robot chooses which path to take, Left, Right, Middle, or Unknown
 
-        switch (goldPosition) {
-            case LEFT:
+            switch (goldPosition) {
+                case LEFT:
                     robot.driveTrain.driveRight(this, 14, defaultSpeed, 3);
                     robot.pause(250);
-                //drive 2" from minerals
+                    //drive 2" from minerals
                     robot.driveTrain.translateForward(this, 21, defaultSpeed, 5);
                     robot.pause(250);
-                //drive to left block
+                    //drive to left block
                     robot.driveTrain.driveRight(this, 10, defaultSpeed, 5);
                     robot.pause(250);
-                //push left block
+                    //push left block
                     robot.driveTrain.translateForward(this, 3, defaultSpeed, 3);
                     robot.pause(250);
-                //Pull back from block
+                    //Pull back from block
                     robot.driveTrain.driveLeft(this, 8, defaultSpeed, 3);
-                //turn robot to the left to face the wall
+                    //turn robot to the left to face the wall
                     robot.gyroAutoDriver.turn(125, 0.25, 5);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateBackward(this, 26, defaultSpeed, 5);
                     robot.pause(250);
-                //drive away from the wall
+                    //drive away from the wall
                     robot.driveTrain.translateForward(this, 5, 0.5, 5);
                     robot.pause(100);
-                //drive to depot
+                    //drive to depot
                     robot.gyroAutoDriver.driveForwards(110, 0.5);
                     robot.pause(250);
-                //deposit marker
+                    //deposit marker
                     robot.markerDeploy.deploy();
                     robot.pause(250);
-                //Release marker
+                    //Release marker
                     robot.driveTrain.driveRight(this, 1, 0.2, 3);
                     robot.pause(250);
-                //retract marker servo
+                    //retract marker servo
                     robot.markerDeploy.retract();
                     robot.pause(250);
-                //drive to crater
+                    //drive to crater
                     robot.gyroAutoDriver.driveBackwards(120, 0.5);
-                break;
+                    break;
 
-            case RIGHT:
+                case RIGHT:
                     robot.driveTrain.driveRight(this, 14, defaultSpeed, 3);
                     robot.pause(250);
-                //drive 2" from minerals
+                    //drive 2" from minerals
                     robot.driveTrain.translateBackward(this, 13.5, 0.2, 5);
                     robot.pause(250);
-                //drive to right block
+                    //drive to right block
                     robot.driveTrain.driveRight(this, 6, defaultSpeed, 5);
                     robot.pause(250);
-                //back up
+                    //back up
                     robot.driveTrain.driveLeft(this, 5, defaultSpeed, 3);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateForward(this, 60, defaultSpeed, 10);
                     robot.pause(250);
-                //turn to be parallel with the wall
+                    //turn to be parallel with the wall
                     robot.gyroAutoDriver.turn(118, 0.25, 5);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateBackward(this, 16, defaultSpeed, 5);
                     robot.pause(250);
-                //drive away from the wall
+                    //drive away from the wall
                     robot.driveTrain.translateForward(this, 5, 0.5, 5);
                     robot.pause(100);
-                //drive to depot
+                    //drive to depot
                     robot.gyroAutoDriver.driveForwards(75, 0.5);
                     robot.pause(500);
-                //deposit marker
+                    //deposit marker
                     robot.markerDeploy.deploy();
                     robot.pause(250);
-                //Release marker
+                    //Release marker
                     robot.driveTrain.driveRight(this, 1, 0.2, 3);
                     robot.pause(250);
-                //retract marker servo
+                    //retract marker servo
                     robot.markerDeploy.retract();
                     robot.pause(250);
-                //drive to crater
+                    //drive to crater
                     //robot.driveLeft(this, 64, 0.5, 10);
                     robot.gyroAutoDriver.driveBackwards(125, 0.5);
-                break;
+                    break;
 
-            case MIDDLE:
-                //Drive to and push middle block
+                case MIDDLE:
+                    //Drive to and push middle block
                     robot.driveTrain.driveRight(this, 24, defaultSpeed, 10);
                     robot.pause(250);
-                //back up
+                    //back up
                     robot.driveTrain.driveLeft(this, 6, defaultSpeed, 3);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateForward(this, 30, defaultSpeed, 10);
                     robot.pause(250);
-                //turn to be parallel with the wall
+                    //turn to be parallel with the wall
                     robot.gyroAutoDriver.turn(115, defaultSpeed, 5);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateBackward(this, 20, defaultSpeed, 5);
                     robot.pause(250);
-                //drive away from the wall
+                    //drive away from the wall
                     robot.driveTrain.translateForward(this, 1, defaultSpeed, 5);
                     robot.pause(250);
-                //drive to depot
+                    //drive to depot
                     robot.gyroAutoDriver.driveForwards(90, 0.5);
                     robot.pause(500);
-                //deposit marker
+                    //deposit marker
                     robot.markerDeploy.deploy();
                     robot.pause(250);
-                //Release marker
+                    //Release marker
                     robot.driveTrain.driveRight(this, 1, 0.2, 3);
                     robot.pause(250);
-                //retract marker servo
+                    //retract marker servo
                     robot.markerDeploy.retract();
                     robot.pause(250);
-                //drive to crater
-                //robot.driveLeft(this, 64, 0.5, 10);
+                    //drive to crater
+                    //robot.driveLeft(this, 64, 0.5, 10);
                     robot.gyroAutoDriver.driveBackwards(125, 0.5);
-                break;
+                    break;
 
-            case UNKNOWN:
-                //Drive to and push middle block
-                robot.driveTrain.driveRight(this, 24, defaultSpeed, 10);
-                robot.pause(250);
-                //back up
-                robot.driveTrain.driveLeft(this, 6, defaultSpeed, 3);
-                robot.pause(250);
-                //drive toward the wall
-                robot.driveTrain.translateForward(this, 30, defaultSpeed, 10);
-                robot.pause(250);
-                //turn to be parallel with the wall
-                robot.gyroAutoDriver.turn(115, defaultSpeed, 5);
-                robot.pause(250);
-                //drive toward the wall
-                robot.driveTrain.translateBackward(this, 20, defaultSpeed, 5);
-                robot.pause(250);
-                //drive away from the wall
-                robot.driveTrain.translateForward(this, 1, defaultSpeed, 5);
-                robot.pause(250);
-                //drive to depot
-                robot.gyroAutoDriver.driveForwards(90, 0.5);
-                robot.pause(500);
-                //deposit marker
-                robot.markerDeploy.deploy();
-                robot.pause(250);
-                //Release marker
-                robot.driveTrain.driveRight(this, 1, 0.2, 3);
-                robot.pause(250);
-                //retract marker servo
-                robot.markerDeploy.retract();
-                robot.pause(250);
-                //drive to crater
-                //robot.driveLeft(this, 64, 0.5, 10);
-                robot.gyroAutoDriver.driveBackwards(125, 0.5);
-                break;
-            default: //like unknown
-                //drive straight and hope the block is there
-                break;
-        }
+                case UNKNOWN:
+//                    //Drive to and push middle block
+//                    robot.driveTrain.driveRight(this, 24, defaultSpeed, 10);
+//                    robot.pause(250);
+//                    //back up
+//                    robot.driveTrain.driveLeft(this, 6, defaultSpeed, 3);
+//                    robot.pause(250);
+//                    //drive toward the wall
+//                    robot.driveTrain.translateForward(this, 30, defaultSpeed, 10);
+//                    robot.pause(250);
+//                    //turn to be parallel with the wall
+//                    robot.gyroAutoDriver.turn(115, defaultSpeed, 5);
+//                    robot.pause(250);
+//                    //drive toward the wall
+//                    robot.driveTrain.translateBackward(this, 20, defaultSpeed, 5);
+//                    robot.pause(250);
+//                    //drive away from the wall
+//                    robot.driveTrain.translateForward(this, 1, defaultSpeed, 5);
+//                    robot.pause(250);
+//                    //drive to depot
+//                    robot.gyroAutoDriver.driveForwards(90, 0.5);
+//                    robot.pause(500);
+//                    //deposit marker
+//                    robot.markerDeploy.deploy();
+//                    robot.pause(250);
+//                    //Release marker
+//                    robot.driveTrain.driveRight(this, 1, 0.2, 3);
+//                    robot.pause(250);
+//                    //retract marker servo
+//                    robot.markerDeploy.retract();
+//                    robot.pause(250);
+//                    //drive to crater
+//                    //robot.driveLeft(this, 64, 0.5, 10);
+//                    robot.gyroAutoDriver.driveBackwards(125, 0.5);
+//                    break;
+                default: //like unknown
+                    //drive straight and hope the block is there
+                    break;
+            }
     }
 }
--- OpModes/Autonomous/DepotAuto.java
+++ OpModes/Autonomous/DepotAuto.java
@@ -30,172 +30,173 @@ public class DepotAuto extends LinearOpMode {
         ElapsedTime timer = new ElapsedTime();
 
         waitForStart();
-        //change to use a more robust function later
-        while(!robot.hangArm.isFullyLowered()) {
-            robot.hangArm.lower();
-        }
-        robot.hangArm.stop();
-        robot.pause(1000);
 
-        //Move robot away from lander latch
-        robot.driveTrain.translateBackward(this, 4, 0.3, 3);
-        robot.pause(500);
+            //change to use a more robust function later
+            while (!robot.hangArm.isFullyLowered()) {
+                robot.hangArm.lower();
+            }
+            robot.hangArm.stop();
+            robot.pause(1000);
 
-        tensor_flow.activate();
-        //FIND GOLD
-        Michaels_tensor_flow.goldfinder goldPosition = Michaels_tensor_flow.goldfinder.UNKNOWN;
-        timer.startTime();
-        while(goldPosition == Michaels_tensor_flow.goldfinder.UNKNOWN) {
-            goldPosition = tensor_flow.getGoldPosition(telemetry);
+            //Move robot away from lander latch
+            robot.driveTrain.translateBackward(this, 4, 0.3, 3);
+            robot.pause(500);
+
+            tensor_flow.activate();
+            //FIND GOLD
+            Michaels_tensor_flow.goldfinder goldPosition = Michaels_tensor_flow.goldfinder.UNKNOWN;
+            timer.reset();
+            while (goldPosition == Michaels_tensor_flow.goldfinder.UNKNOWN && timer.seconds() < 5) {
+                goldPosition = tensor_flow.getGoldPosition(telemetry);
 //            if (timer.seconds() > 5){
 //                break;
 //            }
-        }
-        telemetry.addData("Position", goldPosition);
-        telemetry.update();
-        robot.pause(1000);
-        tensor_flow.deactivate();
+            }
+            telemetry.addData("Position", goldPosition);
+            telemetry.update();
+            robot.pause(1000);
+            tensor_flow.deactivate();
 
-        //Robot chooses which path to take, Left, Right, Middle, or Unknown
+            //Robot chooses which path to take, Left, Right, Middle, or Unknown
 
-        switch (goldPosition) {
-            case LEFT:
+            switch (goldPosition) {
+                case LEFT:
                     robot.driveTrain.driveRight(this, 14, defaultSpeed, 3);
                     robot.pause(250);
-                //drive 2" from minerals
+                    //drive 2" from minerals
                     robot.driveTrain.translateForward(this, 21, defaultSpeed, 5);
                     robot.pause(250);
-                //drive to left block
+                    //drive to left block
                     robot.driveTrain.driveRight(this, 10, defaultSpeed, 5);
                     robot.pause(250);
-                //Pull back from block
+                    //Pull back from block
                     robot.driveTrain.driveLeft(this, 8, defaultSpeed, 3);
                     robot.pause(250);
-                //turn robot to the left to face the wall
+                    //turn robot to the left to face the wall
                     robot.gyroAutoDriver.turn(-30, 0.2, 5);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateForward(this, 26, defaultSpeed, 5);
                     robot.pause(250);
-                //drive away from the wall
+                    //drive away from the wall
                     robot.driveTrain.translateBackward(this, 5, 0.3, 5);
                     robot.pause(100);
-                //drive to depot
+                    //drive to depot
                     robot.gyroAutoDriver.driveForwards(40, 0.5);
                     robot.pause(250);
-                //deposit marker
+                    //deposit marker
                     robot.markerDeploy.deploy();
                     robot.pause(250);
-                //Release marker
+                    //Release marker
                     robot.driveTrain.driveRight(this, 1, 0.2, 3);
                     robot.pause(250);
-                //retract marker servo
+                    //retract marker servo
                     robot.markerDeploy.retract();
                     robot.pause(250);
-                //drive to crater
+                    //drive to crater
                     robot.gyroAutoDriver.driveBackwards(120, 0.5);
-                break;
+                    break;
 
-            case RIGHT:
+                case RIGHT:
                     robot.driveTrain.driveRight(this, 14, defaultSpeed, 3);
                     robot.pause(250);
-                //drive 2" from minerals
+                    //drive 2" from minerals
                     robot.driveTrain.translateBackward(this, 13.5, 0.2, 5);
                     robot.pause(250);
-                //drive to right block
+                    //drive to right block
                     robot.driveTrain.driveRight(this, 6, defaultSpeed, 5);
                     robot.pause(250);
-                //back up
+                    //back up
                     robot.driveTrain.driveLeft(this, 5, defaultSpeed, 3);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateForward(this, 60, defaultSpeed, 10);
                     robot.pause(250);
-                //turn to be parallel with the wall
+                    //turn to be parallel with the wall
                     robot.gyroAutoDriver.turn(-30, 0.20, 5);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateForward(this, 26, defaultSpeed, 5);
                     robot.pause(250);
-                //drive away from the wall
+                    //drive away from the wall
                     robot.driveTrain.translateBackward(this, 5, 0.5, 5);
                     robot.pause(100);
-                //drive to depot
+                    //drive to depot
                     robot.gyroAutoDriver.driveForwards(90, 0.5);
                     robot.pause(500);
-                //deposit marker
+                    //deposit marker
                     robot.markerDeploy.deploy();
                     robot.pause(250);
-                //Release marker
+                    //Release marker
                     robot.driveTrain.driveRight(this, 1, 0.2, 3);
                     robot.pause(250);
-                //retract marker servo
+                    //retract marker servo
                     robot.markerDeploy.retract();
                     robot.pause(250);
-                //drive to crater
+                    //drive to crater
                     robot.gyroAutoDriver.driveBackwards(140, 0.5);
-                break;
+                    break;
 
-            case MIDDLE:
-                //Drive to and push middle block to the depot
+                case MIDDLE:
+                    //Drive to and push middle block to the depot
                     robot.driveTrain.driveRight(this, 55, defaultSpeed, 10);
                     robot.pause(250);
-                //deposit marker
+                    //deposit marker
                     robot.markerDeploy.deploy();
                     robot.pause(250);
-                //Release marker
+                    //Release marker
                     robot.driveTrain.driveRight(this, 1, 0.2, 3);
                     robot.pause(250);
-                //retract marker servo
+                    //retract marker servo
                     robot.markerDeploy.retract();
                     robot.pause(250);
-                //back off of the block to avoid hitting it when turning.
+                    //back off of the block to avoid hitting it when turning.
                     robot.driveTrain.driveLeft(this, 5, 0.2, 3);
                     robot.pause(250);
-                //turn to be parallel with wall
+                    //turn to be parallel with wall
                     robot.gyroAutoDriver.turn(-30, defaultSpeed, 5);
                     robot.pause(250);
-                //drive toward the wall
+                    //drive toward the wall
                     robot.driveTrain.translateForward(this, 20, defaultSpeed, 5);
                     robot.pause(250);
-                //drive away from the wall
+                    //drive away from the wall
                     robot.driveTrain.translateBackward(this, 5, defaultSpeed, 5);
                     robot.pause(250);
-                //drive to crater
+                    //drive to crater
                     robot.gyroAutoDriver.driveBackwards(125, 0.5);
-                break;
+                    break;
 
-            case UNKNOWN:
-                //Drive to and push middle block to the depot
-                robot.driveTrain.driveRight(this, 55, defaultSpeed, 10);
-                robot.pause(250);
-                //deposit marker
-                robot.markerDeploy.deploy();
-                robot.pause(250);
-                //Release marker
-                robot.driveTrain.driveRight(this, 1, 0.2, 3);
-                robot.pause(250);
-                //retract marker servo
-                robot.markerDeploy.retract();
-                robot.pause(250);
-                //back off of the block to avoid hitting it when turning.
-                robot.driveTrain.driveLeft(this, 5, 0.2, 3);
-                robot.pause(250);
-                //turn to be parallel with wall
-                robot.gyroAutoDriver.turn(-30, defaultSpeed, 5);
-                robot.pause(250);
-                //drive toward the wall
-                robot.driveTrain.translateForward(this, 20, defaultSpeed, 5);
-                robot.pause(250);
-                //drive away from the wall
-                robot.driveTrain.translateBackward(this, 5, defaultSpeed, 5);
-                robot.pause(250);
-                //drive to crater
-                robot.gyroAutoDriver.driveBackwards(125, 0.5);
-                break;
-            default: //like unknown
-                //drive straight and hope the block is there
-                break;
+                case UNKNOWN:
+//                    //Drive to and push middle block to the depot
+//                    robot.driveTrain.driveRight(this, 55, defaultSpeed, 10);
+//                    robot.pause(250);
+//                    //deposit marker
+//                    robot.markerDeploy.deploy();
+//                    robot.pause(250);
+//                    //Release marker
+//                    robot.driveTrain.driveRight(this, 1, 0.2, 3);
+//                    robot.pause(250);
+//                    //retract marker servo
+//                    robot.markerDeploy.retract();
+//                    robot.pause(250);
+//                    //back off of the block to avoid hitting it when turning.
+//                    robot.driveTrain.driveLeft(this, 5, 0.2, 3);
+//                    robot.pause(250);
+//                    //turn to be parallel with wall
+//                    robot.gyroAutoDriver.turn(-30, defaultSpeed, 5);
+//                    robot.pause(250);
+//                    //drive toward the wall
+//                    robot.driveTrain.translateForward(this, 20, defaultSpeed, 5);
+//                    robot.pause(250);
+//                    //drive away from the wall
+//                    robot.driveTrain.translateBackward(this, 5, defaultSpeed, 5);
+//                    robot.pause(250);
+//                    //drive to crater
+//                    robot.gyroAutoDriver.driveBackwards(125, 0.5);
+//                    break;
+                default: //like unknown
+                    //drive straight and hope the block is there
+                    break;
+            }
         }
     }
-}
