b075fb4504317c1bcbaa75b02bb362ef910b5a34
FTC9886 <ftc9886@gmail.com>
2019-01-19 10:09:38 -0600

    Fully commented the DriveTrain Subsystem and integrated the Enumeration into all functions

--- Subsystems/DriveTrain.java
+++ Subsystems/DriveTrain.java
@@ -12,16 +12,22 @@ public class DriveTrain {
     public DcMotor  left_back_drive;
     public DcMotor  right_back_drive;
 
+    //Creates an enumeration with values corresponding to the states the drive train can be in.
+    //Forwards is considered to the be side of the robot that the Collector faces.
     private enum DriveTrainEnum{
         FORWARDS,
         BACKWARDS,
         LEFT,
         RIGHT,
+        TURNING,
         STOPPED
     }
 
+    //Creates a new DriveTrainEnum
     private DriveTrainEnum driveTrainState;
 
+    //Defines the DriveTrain object. It is called in the CombinedHardware class/extension where it joins the other subsystems in the robot object and hardware map.
+    //Sets default state of the DriveTrain
     public DriveTrain(String leftFront, String leftBack,String rightFront, String rightBack, HardwareMap hardwareMap){
         this.left_front_drive = hardwareMap.dcMotor.get(leftFront);
         this.left_back_drive = hardwareMap.dcMotor.get(leftBack);
@@ -41,6 +47,8 @@ public class DriveTrain {
         stopDrive();
     }
 
+    //Function that takes a power value and applies it to the drive motors. Used in TeleOp to drive left and right using triggers on a controller.
+    //Considers front of the robot to be side that the Collector is facing
     public void teleTranslate(double power){
         left_front_drive.setPower (power);
         right_front_drive.setPower(power);
@@ -51,7 +59,7 @@ public class DriveTrain {
 
 
 
-
+//Function that resets the motor encoders. Used during autonomous to ensure more consistent results when navigating.
     public void resetEncoders(){
         left_front_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         left_back_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -64,12 +72,14 @@ public class DriveTrain {
         right_back_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    //Defines new ElapsedTime object
     private ElapsedTime period  = new ElapsedTime();
 
     /*Value definitions to be used for distance conversion*/
     static final double     COUNTS_PER_MOTOR_REV    = 1106 ;    // eg: Rev Hex Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 1.1 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 6.0 ;     // For figuring circumference
+    //Double that represents the number of encoder counts per inch of movement for the robot
     public static final double     COUNTS_PER_INCH_LandR   = (COUNTS_PER_MOTOR_REV * 1.042 * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1416);
     public static final double     COUNTS_PER_INCH_FandB   = (COUNTS_PER_MOTOR_REV * 0.958 * DRIVE_GEAR_REDUCTION) /
@@ -94,6 +104,15 @@ public class DriveTrain {
     public void drivePowers(double leftSpeed, double rightSpeed){
         leftDrivePower(leftSpeed);
         rightDrivePower(rightSpeed);
+        if (leftSpeed > 0 && rightSpeed > 0){
+            driveTrainState = DriveTrainEnum.LEFT;
+        } else if(leftSpeed <0 && rightSpeed<0){
+            driveTrainState = DriveTrainEnum.RIGHT;
+        } else if(leftSpeed == 0 && rightSpeed == 0){
+            driveTrainState = DriveTrainEnum.STOPPED;
+        }else {
+            driveTrainState = DriveTrainEnum.TURNING;
+        }
     }
 
     //Function used to set DriveTrain power using joysticks. Intended to be used for tank drive.
@@ -103,6 +122,15 @@ public class DriveTrain {
         right_back_drive.setPower (-rightPower);
         right_front_drive.setPower(rightPower);
         left_back_drive.setPower  (-leftPower);
+        if (leftPower >0 && rightPower>0){
+            driveTrainState = DriveTrainEnum.FORWARDS;
+        } else if (leftPower < 0 && rightPower < 0){
+            driveTrainState = DriveTrainEnum.BACKWARDS;
+        } else if(leftPower == 0 && rightPower == 0){
+            driveTrainState = DriveTrainEnum.STOPPED;
+        }else {
+            driveTrainState = DriveTrainEnum.TURNING;
+        }
     }
 
     //Function that takes a power value and drives the robot forwards. Intended to be used in TeleOp in conjunction with a button or D-pad directoin
@@ -112,7 +140,7 @@ public class DriveTrain {
         right_back_drive.setPower (-power);
         right_front_drive.setPower(power);
         left_back_drive.setPower  (power);
-
+        driveTrainState = DriveTrainEnum.FORWARDS;
     }
 
     //Function that takes a power value and drives the robot backwards. Intended to be used in TeleOp in conjunction with a button or D-pad directoin
@@ -122,35 +150,41 @@ public class DriveTrain {
         right_back_drive.setPower (power);
         right_front_drive.setPower(-power);
         left_back_drive.setPower  (-power);
+        driveTrainState = DriveTrainEnum.BACKWARDS;
     }
 
+    //Function that takes a power value and drives the robot to the left. Intended to be used in TeleOp in conjunction with a button or D-pad directoin
+    //Considers front of the robot to be side of the robot that the Collector faces.
     public void left(double power){
         left_front_drive.setPower (power);
         right_back_drive.setPower (power);
         right_front_drive.setPower(power);
         left_back_drive.setPower  (power);
+        driveTrainState = DriveTrainEnum.LEFT;
     }
 
+    //Function that takes a power value and drives the robot to the right. Intended to be used in TeleOp in conjunction with a button or D-pad directoin
+    //Considers front of the robot to be side of the robot that the Collector faces.
     public void right(double power){
         left_front_drive.setPower (-power);
         right_back_drive.setPower (-power);
         right_front_drive.setPower(-power);
         left_back_drive.setPower  (-power);
-
+        driveTrainState = DriveTrainEnum.RIGHT;
     }
 
 
-    //Stops the DriveTrain
+    //Function that stops the DriveTrain by calling the drivePowers function and passing it 0 speed
     public void stopDrive(){
         drivePowers(0.0, 0.0);
+        driveTrainState = DriveTrainEnum.STOPPED;
     }
 
 
     /*Functions used in current autonomous programs*/
-    public void driveRight (LinearOpMode opMode, double inches, double speed, double timeoutS)
-    {
-        driveLeft(opMode, -inches, speed, timeoutS);
-    }
+
+    //Function that takes a distance in inches, a speed, and a max runtime in seconds and drives the robot to the left. Intended to be used in Autonomous for navigation
+    //Considers front of the robot to be side of the robot that the Collector faces.
     public void driveLeft (LinearOpMode opMode, double inches, double speed, double timeoutS)
     {
         int newLeftFrontTarget  = left_front_drive.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH_LandR);
@@ -180,26 +214,34 @@ public class DriveTrain {
                 (runtime.seconds() < timeoutS) &&
                 (left_front_drive.isBusy() && right_front_drive.isBusy()))
         {
+            if (inches > 0){
+                driveTrainState = DriveTrainEnum.LEFT;
+            } else if (inches <0){
+                driveTrainState = DriveTrainEnum.RIGHT;
+            }
         }
 
         // Stop all motion;
-        left_front_drive.setPower(0);
-        right_front_drive.setPower(0);
-        left_back_drive.setPower(0);
-        right_back_drive.setPower(0);
+        stopDrive();
 
         // Turn off RUN_TO_POSITION
         left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 
-    public void translateBackward(LinearOpMode opMode, double inches, double speed, double timeoutS)
+    //Function that takes a distance in inches, a speed, and a max runtime in seconds and drives the robot to the Right. Intended to be used in Autonomous for navigation
+    //Calls the driveLeft function and passes it reverse distance
+    //Considers front of the robot to be side of the robot that the Collector faces.
+    public void driveRight (LinearOpMode opMode, double inches, double speed, double timeoutS)
     {
-        translateForward(opMode, -inches, speed, timeoutS);
+        driveLeft(opMode, -inches, speed, timeoutS);
     }
 
+    //Function that takes a distance in inches, a speed, and a max runtime in seconds and drives the robot forwards. Intended to be used in Autonomous for navigation
+    //Considers front of the robot to be side of the robot that the Collector faces.
     public void translateForward(LinearOpMode opMode, double inches, double speed, double timeoutS)
     {
         int newLeftFrontTarget  = left_front_drive.getCurrentPosition() + (int)(-inches * COUNTS_PER_INCH_LandR);
@@ -229,6 +271,11 @@ public class DriveTrain {
                 (runtime.seconds() < timeoutS) &&
                 (left_front_drive.isBusy() && right_front_drive.isBusy()))
         {
+            if(inches >0){
+                driveTrainState = DriveTrainEnum.FORWARDS;
+            } else if(inches <0){
+                driveTrainState = DriveTrainEnum.BACKWARDS;
+            }
         }
 
         // Stop all motion;
@@ -241,11 +288,19 @@ public class DriveTrain {
         right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
+    //Function that takes a distance in inches, a speed, and a max runtime in seconds and drives the robot forwards. Intended to be used in Autonomous for navigation
+    //Calls the translateForward function and passes it reverse inches
+    //Considers front of the robot to be side of the robot that the Collector faces.
+    public void translateBackward(LinearOpMode opMode, double inches, double speed, double timeoutS)
+    {
+        translateForward(opMode, -inches, speed, timeoutS);
+    }
 
-
+    //Alternative formula for converting inches into encoder counts for the drivetrain
     protected long inchesToEncoderCounts(double distance){
         return Math.round(((distance / (Math.PI * WHEEL_DIAMETER_INCHES)) / DRIVE_GEAR_REDUCTION) * COUNTS_PER_MOTOR_REV);
     }
+
     @Override
     public String toString(){
         switch (driveTrainState){
@@ -257,6 +312,8 @@ public class DriveTrain {
                 return "Driving Left";
             case RIGHT:
                 return "Driving Right";
+            case TURNING:
+                return "Turning";
             case STOPPED:
                 return "Stopped";
             default:
