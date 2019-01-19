3a2668702d8661b35413e0c848435c56ae23b65d
FTC9886 <ftc9886@gmail.com>
2019-01-19 11:35:41 -0600

    Commented Class

--- Extensions/GyroAutoDriver.java
+++ Extensions/GyroAutoDriver.java
@@ -11,22 +11,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.Extensions.CombinedHardware;
-
+//Class that uses the IMU built into the REV expansion hub to make movement during autonomous more accurate
 public class GyroAutoDriver {
+    //Calls the hardware map
     CombinedHardware hw;
 
+    //Defines the constructor
 public GyroAutoDriver(CombinedHardware hw){
     this.hw = hw;
     angles = hw.adafruitIMU.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YZX, AngleUnit.DEGREES);
 }
-
+//New orientation object
 private Orientation angles;
 
 
-
+//Double that stores the angle the robot is facing
 public double Heading(){
     angles = hw.adafruitIMU.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YZX, AngleUnit.DEGREES);
 
+    //Prevents robot from freaking out when rotating past the 180 degree point
     if(angles.secondAngle == 180)
     {
         angles.secondAngle -= 360;
@@ -34,6 +37,7 @@ public double Heading(){
     return angles.secondAngle;
     }
 
+    //Function that takes a distance in inches and a drive power. Drives backwards that distance, during motion robot automatically adjusts its motor powers down from the set power to maintain a straight path
 public void driveBackwards(float distance, double power){
     double target = Heading();
     hw.driveTrain.resetEncoders();
@@ -49,6 +53,7 @@ public void driveBackwards(float distance, double power){
 
 }
 
+//Function that takes a distance in inches and a drive power. Drives forwards that distance, during motion robot automatically adjusts its motor powers down from the set power to maintain a straight path
 public void driveForwards(float distance, double power){
     double target = Heading();
     hw.driveTrain.resetEncoders();
@@ -66,7 +71,7 @@ public void driveForwards(float distance, double power){
     hw.driveTrain.stopDrive();
 }
 
-
+//Function that takes an angle, a speed, and a max time to turn.
     public void turn(float anglechange, double speed, double timeoutS)
     {
         angles = hw.adafruitIMU.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YZX, AngleUnit.DEGREES);
